f8128e15043926feaaaf3f62ffefc898fddcb2bf
Alex Nepomuceno <sosaalex707@gmail.com>
2023-10-30 14:17:07 -0400

    Final documentation for the project

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -18,16 +18,70 @@ class StockMetrics(StockData):
         """
         averages = []
         for row in self.data:
-            ...
+            valid_prices = []
+            for num in row[1:]:
+                num = num.strip()
+                
+                if num and num.replace('.', '', 1).isdigit():
+                   price = float(num)
+                   valid_prices.append(price)
+            print(valid_prices)
 
+            
+            
+            sum_averages = sum(valid_prices)/ len(valid_prices)
+            averages.append(round(sum_averages,3))
+            print(averages)
+
+                  
+
+                  
+
+                
         return averages
 
     def median02(self):
         """pt2
-        """
+        """ 
+        medians = []
+        for row in self.data:
+            valid_prices = []
+            for num in row[1:]:
+                num = num.strip()
+                
+                if num and num.replace('.', '', 1).isdigit():
+                   price = float(num)
+                   valid_prices.append(price)
+            print(valid_prices)
+
+            
+            
+            sum_medians = stats.median(valid_prices)
+            medians.append(sum_medians)
+            print(medians)
+
+        return medians
         ...
 
     def stddev03(self):
         """pt3
         """
-        ...
+        stddev = []
+        for row in self.data:
+            valid_prices = []
+            for num in row[1:]:
+                num = num.strip()
+                
+                if num and num.replace('.', '', 1).isdigit():
+                   price = float(num)
+                   valid_prices.append(price)
+            print(valid_prices)
+
+            
+            
+            new_stddev = stats.stdev(valid_prices)
+            stddev.append(round(new_stddev, 3))
+            print(stddev)
+
+        return stddev
+       
--- code/test/validate.py
+++ code/test/validate.py
@@ -19,7 +19,7 @@ while in the `code/test` folder to test the completeness of your code.
 The terminal output will tell you if your pipeline is successful.
 """
 import sys
-
+import os 
 
 from code.StockMetrics import StockMetrics
 
